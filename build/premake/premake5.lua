4cbc842af5ebd4561dd2879fe596c8d4e81d7cf9
Daniel Krikun <krikun.daniel@gmail.com>
2016-05-10 22:49:52 +0300

    Fix libyuv path

--- build/premake/premake5.lua
+++ build/premake/premake5.lua
@@ -69,7 +69,7 @@ project "videosrv"
         path.join(ffmpeg_dir, "lib"),
         path.join(sdl_dir, "lib/%{cfg.platform}"),
         x264_dir,
-        path.join(yuv_dir, "cmake-build/%{cfg.platform}")
+        path.join(yuv_dir, "cmake-build/%{cfg.buildcfg}")
     }
 
     filter "configurations:Debug"
